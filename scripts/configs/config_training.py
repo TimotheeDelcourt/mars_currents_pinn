8438d9ff847ba1b7eea418c576725a50d00fd65b
TimotheeDelcourt <timothee.delcourt@eaps.ethz.ch>
2025-10-14 10:02:19 +0200

    Jupiter style, with L1 reg! Lambda: 1e-7 to 5e-5

--- scripts/configs/config_training.py
+++ scripts/configs/config_training.py
@@ -23,6 +23,6 @@ training_config = {
     'num_hidden_layers':1,
     'num_neurons_per_layer':8,
     # 'l1_lambda':[1e-6,1e-5,1e-4,1e-3],
-    'l1_lambda':[1e-2,1e-1,1   ,10],
+    'l1_lambda':[5e-6,5e-5,1e-7],
 }
 
