0af03258087f7cb559c2ac40543ca1bf763f9156
TimotheeDelcourt <timothee.delcourt@eaps.ethz.ch>
2025-10-19 18:30:51 +0200

    Try smoothness regularization, higher lambdas

--- scripts/run_ensemble_training.py
+++ scripts/run_ensemble_training.py
@@ -20,8 +20,8 @@ def run_ensemble_training():
 
     # Bootstrap iteration---------------------------------------------
     # for _ in range(config.training_config['ensemble_size']):
-    for smoothness_lambda in [1e-8,1e-7,1e-6,1e-5,1e-4,1e-3]:
-    # for smoothness_lambda in [1e-2,1e-1,1,10,100,1000]:
+    # for smoothness_lambda in [1e-8,1e-7,1e-6,1e-5,1e-4,1e-3]:
+    for smoothness_lambda in [1e-2,1e-1,1,10,100,1000]:
 
         print('Making folder')
 
