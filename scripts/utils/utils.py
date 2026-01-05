383c79330e44417c82ff5a8f4f1f7307569a0d46
TimotheeDelcourt <timothee.delcourt@eaps.ethz.ch>
2026-01-05 11:28:38 +0100

    sisma launch

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
-import imageio.v3 as iio
+
 import matplotlib.pyplot as plt
 import cmcrameri.cm as cmc
 # import pyshtools as sh
@@ -101,7 +101,7 @@ def fibonacci_sphere(samples,alt,plot=0):
         ax.scatter3D(X,Y,Z, c=Z, cmap='viridis')
         plt.show()
 
-    _,colat, lon = cartesian_to_spherical(tensor[:,0], tensor[:,1], tensor[:,2])
+    _,colat, lon = cartesian_to_spherical_torch(tensor[:,0], tensor[:,1], tensor[:,2])
     lat_deg = np.rad2deg(np.pi/2 - colat)
     lon_deg = np.rad2deg(lon)
     df = pd.DataFrame({'alt':alt*np.ones(samples), 'lat':lat_deg, 'lon':lon_deg})
@@ -233,34 +233,38 @@ def pearson_corrcoef(x, y):
 #         coeffs_sh.to_file('outputs/sh_coefficients/'+output_name+'.sh')
 #     return coeffs_sh
 
-def img2scalar_nocmap(image,vmin=0,vmax=1):
-    '''This function must be used when the colormap of the original figure is not known.
-    "Image" should be a path (e.g., "./figure.png")'''
-    img = iio.imread(image)
-    data = np.dot(img[...,:3]/255, [0.299, 0.587, 0.114])
-    data = vmin + data * (vmax - vmin) # denormalize to match vmin and vmax
-    return data
+try:
+    import imageio.v3 as iio
+    def img2scalar_nocmap(image,vmin=0,vmax=1):
+        '''This function must be used when the colormap of the original figure is not known.
+        "Image" should be a path (e.g., "./figure.png")'''
+        img = iio.imread(image)
+        data = np.dot(img[...,:3]/255, [0.299, 0.587, 0.114])
+        data = vmin + data * (vmax - vmin) # denormalize to match vmin and vmax
+        return data
 
-def img2scalar_cmap(image,cmap,vmin=0,vmax=1):
-    '''This function must be used when the colormap of the original figure is known.
-    "Image" should be a path (e.g., "./figure.png"), and cmap simply a str.'''
-    try:
-        cmap = plt.get_cmap(cmap)
-    except:
-        cmap = cmc.cmaps(cmap)
-    img = iio.imread(image)[...,:3]/255 # RGB in [0,1]
-    H, W, _ = img.shape
-    cmap_colors = cmap(np.linspace(0,1,256))[:,:3]
-    img_flat = img.reshape(-1, 3)
-    dists = ((img_flat[:, None, :] - cmap_colors[None, :, :])**2).sum(axis=2) # Compute Euclidean distance to each color in colormap
+    def img2scalar_cmap(image,cmap,vmin=0,vmax=1):
+        '''This function must be used when the colormap of the original figure is known.
+        "Image" should be a path (e.g., "./figure.png"), and cmap simply a str.'''
+        try:
+            cmap = plt.get_cmap(cmap)
+        except:
+            cmap = cmc.cmaps(cmap)
+        img = iio.imread(image)[...,:3]/255 # RGB in [0,1]
+        H, W, _ = img.shape
+        cmap_colors = cmap(np.linspace(0,1,256))[:,:3]
+        img_flat = img.reshape(-1, 3)
+        dists = ((img_flat[:, None, :] - cmap_colors[None, :, :])**2).sum(axis=2) # Compute Euclidean distance to each color in colormap
 
-    # Identify best match
-    data = np.argmin(dists, axis=1) # array of integers in [0,255]
-    data = data/255.0 # in [0,1]
-    data = vmin + data * (vmax - vmin) # denormalize to match vmin and vmax
-    data = data.reshape(H,W)
-    # print(data)
-    return data
+        # Identify best match
+        data = np.argmin(dists, axis=1) # array of integers in [0,255]
+        data = data/255.0 # in [0,1]
+        data = vmin + data * (vmax - vmin) # denormalize to match vmin and vmax
+        data = data.reshape(H,W)
+        # print(data)
+        return data
+except ImportError:
+    print("imageio.v3 not found, img2scalar_nocmap and img2scalar_cmap functions will not work.")
 
 def mso2lt_lat(ls):
     if not isinstance(ls,int):
