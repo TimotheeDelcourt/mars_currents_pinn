d61fad683ea852681738172a9a483532e3489131
TimotheeDelcourt <timothee.delcourt@eaps.ethz.ch>
2026-01-05 11:48:07 +0100

    euler launch

--- scripts/configs/config_training.py
+++ scripts/configs/config_training.py
@@ -41,9 +41,9 @@ training_config = {
     # 2026/05/01 update, only consider Ls values from now on, no "seasons"
     # 'season_filter':None, # if list, will iterate over all options; options: 'summer', 'winter', 'spring', 'autumn', None, 'summer_autumn', 'autumn_winter', 'winter_spring', 'spring_summer'
     # sisma:
-    'ls_list': [11.25, 33.75, 56.25, 78.75, 101.25, 123.75, 146.25, 168.75, 191.25, 213.75, 236.25, 258.75, 281.25, 303.75, 326.25, 348.75], # 'season_filter' must be not None to use this
+    # 'ls_list': [11.25, 33.75, 56.25, 78.75, 101.25, 123.75, 146.25, 168.75, 191.25, 213.75, 236.25, 258.75, 281.25, 303.75, 326.25, 348.75], # 'season_filter' must be not None to use this
     # euler:
-    # 'ls_list': [22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5],
+    'ls_list': [22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5],
     'ls_angle_band':60, # degrees; default = 60; all data except peak dust storm season = 300
 
     # curstal field condition
